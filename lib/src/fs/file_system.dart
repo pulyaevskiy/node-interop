b4eacbd9941b19dfdaf3b6145fad978f11218252
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2017-06-17 19:32:23 -0700

    Added file headers

--- lib/src/fs/file_system.dart
+++ lib/src/fs/file_system.dart
@@ -1,3 +1,5 @@
+// Copyright (c) 2017, Anatoly Pulyaevskiy. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
 part of node_interop.fs;
 
 class NodeFileSystem implements FileSystem {
