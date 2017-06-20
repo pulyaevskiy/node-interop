4169c38365cc393f8fc65e173b9c5a8e17ea5768
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2017-06-20 07:56:21 -0700

    Update to fs tests

--- node_test/fs_test.dart
+++ node_test/fs_test.dart
@@ -6,6 +6,7 @@ void main() {
     var fs = new NodeFileSystem();
     test('current directory', () {
       expect(fs.currentDirectory, new isInstanceOf<Directory>());
+      expect(fs.currentDirectory.path, isNotEmpty);
       expect(fs.currentDirectory.existsSync(), isTrue);
       expect(fs.currentDirectory.exists(), completion(isTrue));
       expect(fs.currentDirectory.isAbsolute, isTrue);
