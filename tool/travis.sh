81f844e3392a6fc0ca8a55814c37e9e776ea11b1
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-04-18 12:03:12 -0700

    Try low resource mode

--- tool/travis.sh
+++ tool/travis.sh
@@ -15,18 +15,14 @@ fi
 
 if [ "$2" == "node" ]; then
     echo "> pub run build_runner test (dartdevc) ============================="
-    pub run build_runner test --output=build/ -- -r expanded
-
-    rm -rf .dart_tool/build/
-    rm -rf build/
+    pub run build_runner test --output=build/ --low-resources-mode --verbose -- -r expanded
 
     echo "> pub run build_runner test (dart2js) ============================="
-    pub run build_runner test --define="build_node_compilers|entrypoint=compiler=dart2js" --output=build/ -- -r expanded
+    pub run build_runner test --define="build_node_compilers|entrypoint=compiler=dart2js" --output=build/ --low-resources-mode --verbose -- -r expanded
 else
     echo "> pub run test -r expanded ==============================================="
     pub run test -r expanded
 fi
-
 
 echo "> dartfmt -n --set-exit-if-changed . ====================================="
 dartfmt -n --set-exit-if-changed .
