ad953e53fcf2e13d0aeb3fbe90d5b9fa484d4632
Natalie Weizenbaum <nweiz@google.com>
2020-10-28 14:50:45 -0700

    Make Atomics match the JS API

--- node_interop/lib/js.dart
+++ node_interop/lib/js.dart
@@ -41,3 +41,45 @@ abstract class JsError {
   external String get message;
   external String get stack;
 }
+
+/// A namespace for JavaScript's `Atomics` functions.
+@JS()
+abstract class Atomics {
+  @JS('add')
+  external static int add(List<int> typedArray, int index, int value);
+
+  @JS('and')
+  external static int and(List<int> typedArray, int index, int value);
+
+  @JS('compareExchange')
+  external static int compareExchange(
+      List<int> typedArray, int index, int expectedValue, int replacementValue);
+
+  @JS('exchange')
+  external static int exchange(List<int> typedArray, int index, int value);
+
+  @JS('isLockFree')
+  external static bool isLockFree(int size);
+
+  @JS('load')
+  external static int load(List<int> typedArray, int index);
+
+  @JS('notify')
+  external static int notify(List<int> typedArray, int index, [int count]);
+
+  @JS('or')
+  external static int or(List<int> typedArray, int index, int value);
+
+  @JS('store')
+  external static int store(List<int> typedArray, int index, int value);
+
+  @JS('sub')
+  external static int sub(List<int> typedArray, int index, int value);
+
+  @JS('wait')
+  external static String wait(List<int> typedArray, int index, int value,
+      [num timeout]);
+
+  @JS('xor')
+  external static int xor(List<int> typedArray, int index, int value);
+}
